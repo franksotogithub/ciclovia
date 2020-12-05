8392c60a5503a143a1061be6fe0471ece2a9bc12
Frank Soto Pea <fsoto@MMLIMP.GOB.PE>
2020-12-05 17:01:39 -0500

    logs

--- routes/api/file/index.js
+++ routes/api/file/index.js
@@ -17,6 +17,6 @@ router.post('/upload',upload.single('file'),(req,res)=>{
     /*return res.send(req.file);*/
     
 
-    return res.json( {message:'Foto subida correctamente', res:'holass' ,file:req.file });
+    return res.json( {message:'Foto subida correctamente', res:'holass' });
 });
 module.exports = router
