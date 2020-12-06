44a8a3d344f9bc7a29ee0eb207645ce9566209f3
Frank Soto Pea <fsoto@MMLIMP.GOB.PE>
2020-12-06 15:22:06 -0500

    actualizando datos

--- routes/api/element_tramo/index.js
+++ routes/api/element_tramo/index.js
@@ -6,7 +6,7 @@ const controller = require("../../../controller/api/element_tramo/index");
 router.get("", controller.getAllElementTramo) //api
 router.get("/:id", controller.getElementTramo) //api
 router.post("", controller.createElementTramo) //api
-router.put("", controller.updateElementTramo) //api
+router.put("/:id", controller.updateElementTramo) //api
 router.delete("",controller.deleteElementTramo)
 
 module.exports = router
